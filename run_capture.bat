2e91a878770dec4b5e0edd790a6e4bc55e59ffce
gwillz <gwillz@internode.on.net>
2015-07-27 11:31:55 +0930

    update .bat to match reworked script

--- run_capture.bat
+++ run_capture.bat
@@ -1,3 +1,3 @@
 @echo off
 start /b cmd /c "vicon_binaries\ViconDataStreamServer.exe & pause"
-start /b cmd /c "C:\Python27\python.exe vicon_capture.py EE1 --time 1 --config unisa-ml_vicon.cfg & pause"
+C:\Python27\python.exe vicon_capture.py --time 1 --config unisa-ml_vicon.cfg
