1ee11052964629c790a7b3d2f9be0e81f57e928f
gwillz <gwillz@internode.on.net>
2015-09-03 19:04:08 +0930

    added subjects access functions

--- python_vicon/Vicon_python.cpp
+++ python_vicon/Vicon_python.cpp
@@ -1,42 +1,77 @@
 #include <Python.h>
+#include <string>
 #include "Client.h"
- 
-static ViconDataStreamSDK::CPP::Client vicon_client;
+
+using namespace ViconDataStreamSDK;
+using namespace CPP;
 
 static PyObject* pyvicon_connect(PyObject* self, PyObject* args) {
-    //PyObject* ret;
+    Client* client = (Client*)PyCapsule_Import("pyvicon.client", 0); 
     char* input;
     
     // parse arguments
-    if (!PyArg_ParseTuple(args, "s", &input)) {
+    if (!PyArg_ParseTuple(args, "s", &input))
         return NULL;
-    }
     
-    vicon_client.Connect(input);
+    // run connect if not connected, error handling..?
+    if (!client->IsConnected().Connected)
+        client->Connect(input);
     return NULL;
 }
 
 static PyObject* pyvicon_disconnect(PyObject* self, PyObject* args) {
-    ViconDataStreamSDK::CPP::Output_IsConnected stat = vicon_client.IsConnected();
-    if (stat.Connected)
-        vicon_client.Disconnect();
-    return Py_None;
+    Client* client = (Client*)PyCapsule_Import("pyvicon.client", 0);
+    
+    //disconnect - if connected
+    if (client->IsConnected().Connected)
+        client->Disconnect();
+    return NULL;
 }
 
 static PyObject* pyvicon_isconnected(PyObject* self, PyObject* args) {
-    ViconDataStreamSDK::CPP::Output_IsConnected stat = vicon_client.IsConnected();
-    return stat.Connected ? Py_True : Py_False;
+    Client* client = (Client*)PyCapsule_Import("pyvicon.client", 0);
+    
+    // status, as pybool
+    return client->IsConnected().Connected ? Py_True : Py_False;
 }
 
-static PyMethodDef ModuleMethods[] =
-{
+static PyObject* pyvicon_subjectcount(PyObject* self, PyObject* args) {
+    Client* client = (Client*)PyCapsule_Import("pyvicon.client", 0);
+    
+    unsigned int sub_count = client->GetSubjectCount().SubjectCount;
+    return Py_BuildValue("I", sub_count);
+}
+
+static PyObject* pyvicon_subjectname(PyObject* self, PyObject* args) {
+    Client* client = (Client*)PyCapsule_Import("pyvicon.client", 0);
+    unsigned int index;
+    
+    if (!PyArg_ParseTuple(args, "I", index))
+        return NULL;
+    
+    //cast from the silly vicon string type
+    std::string sub_name = (std::string)client->GetSubjectName(index).SubjectName;
+    
+    return Py_BuildValue("s", sub_name.c_str());
+}
+
+//declare the accessible functions
+static PyMethodDef ModuleMethods[] = {
      {"connect", pyvicon_connect, METH_VARARGS, "Connect to vicon"},
      {"disconnect", pyvicon_disconnect, METH_NOARGS, "Disconnect from vicon"},
      {"isconnected", pyvicon_isconnected, METH_NOARGS, "Connection status"},
+     {"subjectcount", pyvicon_subjectcount, METH_NOARGS, "Get a count of subjects"},
      {NULL, NULL, 0, NULL},
 };
 
 //DL_EXPORT(void) initpyvicon(void) {
 PyMODINIT_FUNC initpyvicon(void) {
-     (void) Py_InitModule("pyvicon", ModuleMethods);
+    //create the module
+    PyObject* m;
+    m = Py_InitModule("pyvicon", ModuleMethods);
+    
+    //create vicon client and add to the module - inside a capsule
+    Client* vicon_client = new Client();
+    PyObject* pyclient = PyCapsule_New(vicon_client, "pyvicon.client", NULL);
+    PyModule_AddObject(m, "client", pyclient);
 }
--- python_vicon/run_setup.bat
+++ python_vicon/run_setup.bat
@@ -1,15 +1,17 @@
 @echo off
 
-:: VS2010
+:: Set VS version
 ::SET VS90COMNTOOLS=%VS100COMNTOOLS%
-:: VS2012
 SET VS90COMNTOOLS=%VS110COMNTOOLS%
-:: VS2013
-::SET VS90COMNTOOLS=%VS120COMNTOOLS
+::SET VS90COMNTOOLS=%VS120COMNTOOLS%
 
 ::setenv /x64 /release
 set MSSDK=1
 set DISTUTILS_USE_SDK=1
 
 python setup.py build
+if errorlevel 0 (
+    copy build\lib.win-amd64-2.7\pyvicon.pyd .\
+)
+echo %errorlevel%
 pause
